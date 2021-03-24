a7161738366a77e10004929ef0c27a247b6be6a6
YuraLavrinov <123143.hF>
2021-03-24 15:26:35 +0300

    Du v3.0

--- src/main/java/FileParameters.java
+++ src/main/java/FileParameters.java
@@ -5,18 +5,17 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-
 
 public class FileParameters {
     private final boolean h;
     private final boolean c;
     private final boolean si;
-    private double sum = 0;
-    private int flag;
     private final List<String> files;
+
 
     public FileParameters(boolean h, boolean c, boolean si, List<String> files) {
         this.h = h;
@@ -27,49 +26,53 @@ public class FileParameters {
 
     public void sizeOfFiles(OutputStream outputStream) {
 
-        String[] sizeS = {"B", "KB", "MB", "GB"};
+        List<String> result = new ArrayList<>();
         double hu = si ? 1000 : 1024;
+        double size;
+        Pair<Double, String> pair;
+        double sum = 0;
 
-        try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
-            double size;
-            for (String element : files) {
+        for (String element : files) {
 
-                File file = new File(element);
+            File file = new File(element);
 
-                if (!file.exists()) {
-                    System.err.println("invalid file name/path or it doesn't exist");
-                    return;
-                }
-
-                if (file.isDirectory()) size = FileUtils.sizeOfDirectory(file);
-                else size = file.length();
-
-                if (c) sum += size;
-
-                size = humanView(size, hu);
-
-                outputStreamWriter.write("Size of " + element + " " +
-                        String.format("%.3f", size) + " " + sizeS[flag] + '\n');
-
+            if (!file.exists()) {
+                System.err.println("invalid file name/path or it doesn't exist");
+                return;
             }
-            size = humanView(sum, hu);
-            if (c) outputStreamWriter.write("Sum of all " + String.format("%.3f", size) + " " + sizeS[flag]);
 
-            outputStreamWriter.close();
+            if (file.isDirectory()) size = FileUtils.sizeOfDirectory(file);
+            else size = file.length();
+
+            if (c) sum += size;
+
+            pair = humanView(size, hu);
+
+            result.add("Size of " + element + " " + String.format("%.3f", pair.getFirst())
+                    + " " + pair.getSecond() + '\n');
+        }
+        pair = humanView(sum, hu);
+        if (c) result.add("Sum of all " + String.format("%.3f", pair.getFirst())
+                + " " + pair.getSecond());
+
+        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+            for (String element : result) {
+                outputStreamWriter.write(element);
+            }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    public double humanView(double len, double system) {
-        flag = 0;
+
+    public Pair<Double, String> humanView(double len, double system) {
+        String[] sizeS = {"B", "KB", "MB", "GB"};
+        int flag = 0;
         do {
             len /= system;
             flag++;
             if (!h) break;
         } while (len > system);
-        return len;
+        return new Pair<>(len, sizeS[flag]);
     }
 }
-
